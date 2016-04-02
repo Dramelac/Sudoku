38a754112c50b260ae613690c6bee205503aa3be
Antoine SIMONIN <209745@supinfo.com>
2016-04-02 16:31:21 +0200

    pop up done

--- workspace/Sudoku/src/com/supinfo/window/DialogBox.java
+++ workspace/Sudoku/src/com/supinfo/window/DialogBox.java
@@ -1,5 +1,6 @@
 package com.supinfo.window;
 
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -13,17 +14,19 @@ public class DialogBox extends JFrame implements ActionListener{
 	
 
 	private JPanel pan = new JPanel();
-	private String[] messageInt = {"Easy", "Medium", "Hard"};
+	private String[] messageInt = {"1","2","3","4","5","6","7","8","9"};
 	private JComboBox<String> cmbIntList = new JComboBox<String>(messageInt);
 	private JButton buttonYes = new JButton("Yes");
 	private JButton buttonNo = new JButton("No");
 	private JLabel enterNumber = new JLabel("Enter a number");
+	
 
 	public DialogBox(){
-		this.setSize(480, 600);
+		this.setSize(200, 138);
 		this.setVisible(true);
-		
+		this.setLocation(140,231);
 		this.generateGraphic();
+		
 		
 		
 	}
@@ -32,10 +35,10 @@ public class DialogBox extends JFrame implements ActionListener{
 		pan.setLayout(null);
 		
 		
-		cmbIntList.setBounds(220, 20, 100, 25);	  
-		buttonNo.setBounds(350, 20, 100, 25);
-		buttonYes.setBounds(350, 20, 100, 25);	
-		enterNumber.setBounds(100, 50, 60, 30);
+		cmbIntList.setBounds(75, 35, 50, 25);	  
+		buttonNo.setBounds(92, 75, 92, 25);
+		buttonYes.setBounds(0, 75, 92, 25);	
+		enterNumber.setBounds(50, 0, 180, 30);
 		
 		pan.add(cmbIntList);
 		pan.add(buttonNo);
