55cb478c509c7caf75ef9bd018d47a31bf030eef
Antoine SIMONIN <209745@supinfo.com>
2016-04-02 15:48:55 +0200

    up

--- workspace/Sudoku/src/com/supinfo/window/DialogBox.java
+++ workspace/Sudoku/src/com/supinfo/window/DialogBox.java
@@ -14,13 +14,33 @@ public class DialogBox extends JFrame implements ActionListener{
 	private JComboBox<String> cmbIntList = new JComboBox<String>(messageInt);
 	private JButton buttonYes = new JButton("Yes");
 	private JButton buttonNo = new JButton("No");
-	private JLabel lblText = new JLabel();
+	private JLabel enterNumber = new JLabel("Enter a number");
 
 	public DialogBox(String arg){
 		this.setTitle(arg);
 		this.setSize(480, 600);
 		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.generateGraphic();
+		
+		
 	}
-	
+	public void generateGraphic(){
+		 
+		pan.setLayout(null);
+		
+		
+		cmbIntList.setBounds(220, 20, 100, 25);	  
+		buttonNo.setBounds(350, 20, 100, 25);
+		buttonYes.setBounds(350, 20, 100, 25);	
+		enterNumber.setBounds(100, 50, 60, 30);
+		
+		pan.add(cmbIntList);
+		pan.add(buttonNo);
+		pan.add(buttonYes);
+		pan.add(enterNumber);
+		
+		this.setContentPane(pan);
+	}
+
 }
